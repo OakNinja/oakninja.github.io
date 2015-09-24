4496c632793bdb1bbd6fef5b66a54ffa47a09af4
OakNinja <oakninja@gmail.com>
2015-09-24 23:23:06 +0200

    Updated Easing plugin

--- js/paraflax.js
+++ js/paraflax.js
@@ -1,5 +1,8 @@
 /*!
- * From Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ *
+ * Paraflax Bootstrap Theme
+ * 
+ * Based on Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
