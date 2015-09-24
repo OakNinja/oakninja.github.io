56cece39600f3095804c388cf7c50c6b835ade9a
OakNinja <oakninja@gmail.com>
2015-09-25 00:11:16 +0200

    Footer fixes, minified versions of js

--- js/paraflax.js
+++ js/paraflax.js
@@ -1,12 +1,3 @@
-/*!
- *
- * Paraflax Bootstrap Theme
- * 
- * Based on Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
@@ -31,3 +22,7 @@ $(function() {
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+// Parallax
+var scene = document.getElementById('scene');
+var parallax = new Parallax(scene);
