d7e547337854a7076badbb36731355433d6cea81
Esse Woods <oakninja@gmail.com>
2020-09-07 21:24:31 +0200

    Updates

---
+++ precache-manifest.bcc132cc3b13245a69d49b160603d819.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048482cada1a51fab4dd65a62b782f37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf9e9f0a2be5ca1f0e16",
+    "url": "/static/css/main.22c3b838.chunk.css"
+  },
+  {
+    "revision": "19f68df2d7801898eb73",
+    "url": "/static/js/2.27219765.chunk.js"
+  },
+  {
+    "revision": "22d134a87356516cb9f351e71fce83b5",
+    "url": "/static/js/2.27219765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9e9f0a2be5ca1f0e16",
+    "url": "/static/js/main.a230b620.chunk.js"
+  },
+  {
+    "revision": "dd309a82669c53f84a90",
+    "url": "/static/js/runtime-main.df066e79.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
